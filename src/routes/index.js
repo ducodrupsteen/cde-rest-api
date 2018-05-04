8fc1cc1e88121db6f58ba922c92825373e839aaf
DucoDR <drupsteen.duco@gmail.com>
2018-05-04 18:59:27 +0000

    Package 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import config from '../config';
+// import config from '../config';
 import initializeDatabaseConnection from '../config/db';
 import tasks from '../services/tasks';
 import content from '../services/content';
 import log from '../log';
+
+const config = {
+  port: process.env.PORT,
+  mongoUrl: process.env.MONGODBURL,
+  bodyLimit: '1000kb'
+}
 
 const router = express()
 
