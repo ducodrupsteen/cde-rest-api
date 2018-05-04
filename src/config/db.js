00b788b63e50cddc0230c8655729a73dc341726e
DucoDR <drupsteen.duco@gmail.com>
2018-05-04 16:08:57 +0000

    connection to mlab db tryout

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ export default function initializeDatabaseConnectino(callback) {
   mongoose.Promise = global.Promise
 
   return new Promise((resolve, reject) => {
-    const db = mongoose.connect(config.mongoUrl, {
+    const db = mongoose.connect(process.env.DEV_MONGODB, {
       useMongoClient: true
     })
     return resolve(db)
