bde71ba8cbed87f7f46ab066572dbdf3437168af
info@noudadrichem.com <info@noudadrichem.com>
2018-05-15 14:54:30 +0200

    fixed some lint errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
-import passport from 'passport';
 import config from './config';
 import routes from './routes';
 import log from './log';
 import middleware from './middlewares';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import validator from 'express-validator'
 
 const app = express();
-const LocalStrategy = require('passport-local').Strategy;
 
 app.server = http.createServer(app);
 
