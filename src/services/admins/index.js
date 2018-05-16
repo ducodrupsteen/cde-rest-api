ccb6c5731fea1ea932dcf6aa1d77a5772cbfc3c3
DucoDR <drupsteen.duco@gmail.com>
2018-05-16 15:18:04 +0200

    changes for heroku

--- src/services/admins/index.js
+++ src/services/admins/index.js
@@ -2,22 +2,21 @@ import { Router as router } from 'express'
 import Content from '../../models/admins'
 import actions from './actions.js'
 
-<<<<<<< HEAD
 export default function adminController({ config, log, verify }){
   const admins = router()
 
   admins.post('/register', verify.verifyToken, actions.registerAdmin)
-=======
+
 const {
   registerAdmin,
   loginAdmin
-} = actions
+}
 
 export default function adminController() {
   const admins = router()
 
   admins.post('/register', registerAdmin)
->>>>>>> c9107356005577bba30e7d5ce1e439a9b42a3258
+
 
   admins.post('/login', loginAdmin)
 
