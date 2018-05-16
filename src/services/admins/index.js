7ade7600d645e4076ed127e418947d317ae761da
DucoDR <drupsteen.duco@gmail.com>
2018-05-16 15:23:16 +0200

    merge fail at admincontroller

--- src/services/admins/index.js
+++ src/services/admins/index.js
@@ -2,21 +2,15 @@ import { Router as router } from 'express'
 import Content from '../../models/admins'
 import actions from './actions.js'
 
-export default function adminController({ config, log, verify }){
-  const admins = router()
-
-  admins.post('/register', verify.verifyToken, actions.registerAdmin)
-
 const {
   registerAdmin,
   loginAdmin
-}
+} = actions
 
 export default function adminController() {
   const admins = router()
 
   admins.post('/register', registerAdmin)
-
 
   admins.post('/login', loginAdmin)
 
